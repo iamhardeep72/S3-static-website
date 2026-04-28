f223a3b82d1e481fccd1d62f24635e960bc05d0f
Hardeep singh <hardeepsinghgora7288@gmail.com>
2026-04-28 15:52:04 +0530

    Delete src/src directory

--- src/src/script.js
+++
@@ -1,6 +0,0 @@
-function checkSystem() {
-    const status = document.getElementById('status');
-    status.innerText = "System Secure - No Threats Found";
-    status.style.color = "#3fb950";
-    alert("Scan Complete!");
-}
